d5857e6e8a6c8b576e9c105ccbe6d91c9fb33f40
Michael9051 <atlet999@gmail.com>
2025-09-06 22:30:31 +0200

    new file:   .history/next.config_20250906054846.ts 	new file:   .history/next.config_20250906222831.ts 	modified:   next.config.ts 	new file:   package-lock.json

--- .history/next.config_20250906054846.ts
+++ .history/next.config_20250906054846.ts
@@ -6,8 +6,6 @@ const nextConfig: NextConfig = {
   /* config options here */
   experimental: {
     cssChunking: true, // default
-    workerThreads: false,
-    cpus: 1,
   },
 };
 export default nextConfig;
